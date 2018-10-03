54082ea0cd9ea4f9059e1e42e549b98f2cd79f37
Vishal Patel <vppatel@ualberta.ca>
2018-10-02 23:03:27 -0600

    Completed base for Main Activity UI.

---
+++ app/src/main/java/com/example/vishal/vppatel_feelsbook/DisplayHistoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.vishal.vppatel_feelsbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_history);
+    }
+}
